83adedc3644ea39f987e465bb5e4e5657254ed60
Papinutti <32128692+Papinutti@users.noreply.github.com>
2019-06-27 17:07:13 +0200

    Update index.php

--- index.php
+++ index.php
@@ -1,14 +1,1 @@
-<?php
-	session_start();
-
-	// Check if session is active, if a user tries to access the website withut log in,
-	// redirect to the log in
-	if (!isset($_SESSION["AccountID"])) {
-		// Redirect to LogIn
-		header("Location: login/Login.php");
-	}
-	else{
-		// Redirect to LogIn
-		header("Location: about_us/AboutUs.php");
-	}
-?>
+Hello
